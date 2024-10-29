1698bcbb97dfb77466b1ca51bc283349f927337c
tapankumar003 <tapankumar29808@gmail.com>
2024-10-29 21:54:02 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'; // 🔥 Make sure to import the App.css file.
 
-// Implement the User component separately
-function User(props) {
+function Users(props) {
   return (
-    <div>{props.user.age}Age - {props.user.name}</div>
-  );
+    <div className='mcard'>
+  <div><h5>{props.user.name}</h5> {props.user.des}</div>
+  <button onClick={() => props.handleDelete(props.user.id)}>
+        Delete
+      </button>
+      <button onClick={() => props.handleDelete(props.user.id)}>
+        Done
+      </button>
+
+  </div>
+);
 }
 
 function App() {
-  const users = [
-    { id: 1, age: 30, name: "Brad Pitt" },
-    { id: 2, age: 24, name: "Tom Holland" },
-    { id: 3, age: 21, name: "Zendaya" },
-    { id: 4, age: 29, name: "Shah Rukh Khan" },
-  ];
-  return (
-    <div className="app-container">
-      {users.map((user) => {
-        return <User user={user} key={user.id} />;
-      })}
-    </div>
-  );
-}
+  const [users, setUsers] = useState(
+    [{ id: 1, des: 'hyper text mark up language...', name: 'Html' },
+    { id: 2, des: 'cascading styling sheet..', name: 'CSS' }
+    ]);
 
-export default App;
+    const [doneitems, setdone] = useState([]);
+
+  const addUserHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      des: des,
+      name: name
+    };
+    setUsers([...users,newUser])
+  };
+
+  const deleteUserHandler = (id) => {
+    const newUserList = users.filter((user) => user.id !== id);
+    const doneitem = users.filter((user) => user.id === id)[0];
+    setUsers(newUserList);
+    setdone([...doneitems,doneitem])
+  };
+
+  const [name, setName] = useState('');
+  const [des, setdes] = useState('');
+
+
+  return (
+    <div className='maincard'>
+      <h2 className='hdn'>My Todo List</h2>
+      <div className='unp'>
+      <h5>Title: </h5><input className='inp' value={name}
+        onChange={(e) => setName(e.target.value)} />
+
+      <h5>description: </h5><input className='inp' value={des} 
+        onChange={(e) => setdes(e.target.value)} />
+
+      <button className='ibtn' onClick={addUserHandler}>Add</button>
+      </div >
+      <h3>working</h3>
+      <div className='mmcd'>
+      {users.map((user) => {
+        return (<Users  user={user} key={user.id} handleDelete={deleteUserHandler}></Users>);
+      })}
+      </div>
+      <hr/>
+      <h3>done</h3>
+      <div className='mmcd'>
+      {doneitems.map((doneitem) => {
+        return (<Users  user={doneitem} key={doneitem.id} handleDelete={deleteUserHandler}></Users>);
+      })}
+      </div>
+    </div>
+
+  );
+} export default App;
